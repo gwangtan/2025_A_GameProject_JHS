311643e4a6fab67ea381d582e24781ddf246b227
gwangtan <>
2025-11-24 15:21:52 +0900

    11_24

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -38,6 +38,8 @@ public class PlayerController : MonoBehaviour
 
     private bool isUIMode = false;
 
+    public GameObject effectPos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -144,10 +146,12 @@ public class PlayerController : MonoBehaviour
             // ÂøÁö ¸ğ¼Ç Æ®¸®°Å ¹× ÂøÁö »óÅÂ ½ÃÀÛ
             if (!wasGrounded && animator != null)
             {
-                animator.SetTrigger("landTrigger");
+                //animator.SetTrigger("landTrigger"); // This line is commented out
                 isLanding = true;
                 landingTimer = landingDuration;
-                Debug.Log("ÂøÁö");
+                Debug.Log("ÂøÁö"); // Debug.Log("Landing")
+
+                EffectManager.Instance.PlayEffect("ÂøÁöÀÌÆåÆ®", transform.position);
             }
         }
     }
@@ -183,9 +187,10 @@ public class PlayerController : MonoBehaviour
             if(animator != null)
             {
                 animator.SetTrigger("attackTrigger");
-
+                EffectManager.Instance.PlayEffectWithDelay("°ø°İÀÌÆåÆ®", effectPos.transform.position, Quaternion.identity, 0.5f, 1.0f);
             }
         }
+        
     }
 
     void HandleJump()
